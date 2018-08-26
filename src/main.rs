78fa59ea51e7cea8231a6c5597e4e0dfe6667feb
PaKo <pako8128@protonmail.com>
2018-08-26 23:05:16 +0200

    there is something that compiles

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,57 @@
 #[macro_use]
 extern crate clap;
 extern crate hyper;
+extern crate futures;
 
 use clap::App;
 
 use hyper::Client;
-use hyper::rt::{self, Future, Stream}; // hyper re-exposes the traits from future crate
+use hyper::rt; // hyper re-exposes the traits from future crate
+
+use futures::{stream, Future, Stream};
 
 use std::io::prelude::*;
 use std::io;
-use std::fs::File;
 
 fn main() {
 	// parse the command line arguments
 	let yaml = load_yaml!("../args.yml");
 	let matches = App::from_yaml(yaml).get_matches();
 	
-	let url = match matches.value_of("URL") {
-	    Some(url) => url,
-	    None => "http://example.com",
+	let urls = match matches.values_of("URLS") {
+	    Some(urls) => urls.collect(),
+	    None => vec!["http://example.com"],
 	};
 
-	let url = url.parse().unwrap();
-	
+	let urls = urls.into_iter().map(|url| url.parse().unwrap()).collect();
+
+    
 	// using the tokio runtime
-	rt::run(fetch_url(url));
+	rt::run(fetch_urls(urls));
 }
 
- 
-fn fetch_url(url: hyper::Uri) -> impl Future<Item=(), Error=()> {
-    let client = Client::new();
+fn fetch_urls(urls: Vec<hyper::Uri>) -> impl Future<Item=(), Error = ()> {
+    stream::iter_ok(urls)
+        .for_each(|url| fetch_url(url))
+}
 
+fn fetch_url(url: hyper::Uri) -> impl Future<Item = (), Error = ()> {
+    let client = Client::new();
+        
     client
         .get(url)
         .and_then(|res| {
-            println!("Response: {}", res.status());
-            println!("Headers: {:#?}", res.headers());
+            println!("Status: {}", res.status());
+            println!("Header: {:#?}", res.headers());
 
-            res.into_body().for_each(|chunk| {
-                io::stdout().write_all(&chunk)
-                    .map_err(|e| panic!("example expects stdout is open, error={}", e))
-            })
-        })
-        .map(|_| {
-            println!("\n\nDone.");
+            res.into_body()
+                .for_each(|chunk| {
+                    io::stdout()
+                    .write_all(&chunk)
+                    .map_err(|_| panic!("no"))
+                })
         })
         .map_err(|err| {
-            eprintln!("Error {}", err);
+            println!("Error: {:?}", err);
         })
 }
