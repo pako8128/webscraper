289df45aa737ec8515c63b3426cf2663707ac6f8
PaKo <pako8128@protonmail.com>
2018-08-27 00:53:29 +0200

    parsing output directory from command-line and writing pages to file

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,8 @@ use hyper::client::HttpConnector;
 use futures::{stream, Future, Stream};
 
 use std::io::prelude::*;
-use std::io;
+use std::path::PathBuf;
+use std::fs::{File, DirBuilder};
 
 fn main() {
 	// parse the command line arguments
@@ -24,32 +25,49 @@ fn main() {
 	    None => vec!["http://example.com"],
 	};
 
-	let urls = urls.into_iter().map(|url| url.parse().unwrap()).collect();
+    let output_dir = match matches.value_of("output") {
+        Some(path) => PathBuf::from(path),
+        None => PathBuf::from("."),
+    };
 
+    // create the directory if it doesn't exist already
+    if !output_dir.exists() {
+        DirBuilder::new().recursive(true).create(&output_dir).unwrap();
+    }
     
+	let urls = urls.into_iter().map(|url| url.parse().unwrap()).collect();
+   
 	// using the tokio runtime
-	rt::run(fetch_urls(urls));
+	rt::run(fetch_urls(urls, output_dir));
 }
 
-fn fetch_urls(urls: Vec<hyper::Uri>) -> impl Future<Item=(), Error = ()> {
+fn fetch_urls(urls: Vec<hyper::Uri>, output_dir: PathBuf) -> impl Future<Item=(), Error = ()> {
     let client = Client::new();
 
     stream::iter_ok(urls)
-        .for_each(move |url| fetch_url(url, &client))
+        .for_each(move |url| {
+            let mut output_dir = output_dir.join(url.path().split('/').last().unwrap());
+            if output_dir.is_dir() {
+                output_dir.push("index.html");
+            }
+            println!("output_dir: {:?}", output_dir);
+            let output = File::create(output_dir).unwrap();
+            fetch_url(url, &client, output)
+        })
 }
 
-fn fetch_url(url: hyper::Uri, client: &Client<HttpConnector>) -> impl Future<Item = (), Error = ()> {
+fn fetch_url(url: hyper::Uri, client: &Client<HttpConnector>, mut output: File) -> impl Future<Item = (), Error = ()> {
     client
         .get(url)
-        .and_then(|res| {
+        .and_then(move |res| {
             println!("Status: {}", res.status());
             println!("Header: {:#?}", res.headers());
 
             res.into_body()
-                .for_each(|chunk| {
-                    io::stdout()
+                .for_each(move |chunk| {
+                    output
                     .write_all(&chunk)
-                    .map_err(|_| panic!("no"))
+                    .map_err(|_| panic!("oops"))
                 })
         })
         .map_err(|err| {
